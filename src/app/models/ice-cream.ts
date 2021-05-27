b87aa15c3a82c8a53626800f2b932b2fd47c8e8b
Lovisa Bohman <lovisabo@hotmail.com>
2021-05-27 13:26:37 +0200

    Add description to model

--- src/app/models/ice-cream.ts
+++ src/app/models/ice-cream.ts
@@ -1,9 +1,13 @@
+import { stringify } from "@angular/compiler/src/util";
+
 export class IceCream {
   name: string;
   image: string;
+  description: string;
 
-  constructor(name: string, image: string) {
+  constructor(name: string, image: string, description: string) {
     this.name = name;
     this.image = image;
+    this.description = description;
   }
 }
