e966031fdf7605723d84333058d5dc9e8836fc0c
Lovisa Bohman <lovisabo@hotmail.com>
2021-05-27 13:29:22 +0200

    Add the description to the icecreams, now just dummy text

--- src/app/services/ice-cream.service.ts
+++ src/app/services/ice-cream.service.ts
@@ -11,33 +11,35 @@ export class IceCreamService {
 
   constructor() {}
   getIceCream() {
-    this.icecreams.push(new IceCream('88', 'assets/glassbilder/88.png'));
     this.icecreams.push(
-      new IceCream('Ben And Jerrys', 'assets/glassbilder/benAndJerrys.jpg')
+      new IceCream('88', 'assets/glassbilder/88.png', 'Dummy text')
+      );
+    this.icecreams.push(
+      new IceCream('Ben And Jerrys', 'assets/glassbilder/benAndJerrys.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Calippo', 'assets/glassbilder/calippoCola.jpg')
+      new IceCream('Calippo', 'assets/glassbilder/calippoCola.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Cornetto', 'assets/glassbilder/cornetto.jpg')
+      new IceCream('Cornetto', 'assets/glassbilder/cornetto.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Magnum', 'assets/glassbilder/magnum.jpg')
+      new IceCream('Magnum', 'assets/glassbilder/magnum.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Magnum Mandel', 'assets/glassbilder/magnumMandelPaket.jpg')
+      new IceCream('Magnum Mandel', 'assets/glassbilder/magnumMandelPaket.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Piggelin', 'assets/glassbilder/piggelin.jpg')
+      new IceCream('Piggelin', 'assets/glassbilder/piggelin.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Sandwich', 'assets/glassbilder/sandwich.png')
+      new IceCream('Sandwich', 'assets/glassbilder/sandwich.png', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('TipTop', 'assets/glassbilder/tiptop.jpg')
+      new IceCream('TipTop', 'assets/glassbilder/tiptop.jpg', 'Dummy text')
     );
     this.icecreams.push(
-      new IceCream('Twister', 'assets/glassbilder/twister.jpg')
+      new IceCream('Twister', 'assets/glassbilder/twister.jpg', 'Dummy text')
     );
 
     return this.icecreams;
